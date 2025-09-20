dedfb357c58955672ab7c5a112d61c818fbe0c6e
Md. Aiful Alam Mahim <arifulalam7865@gmail.com>
2025-09-20 22:53:28 +0600

    trying to solve merge conflict

--- main.java
+++ main.java
@@ -1,13 +1,13 @@
 public class main {
     public static void main(String[] args) {
-        Student student = new Student("B", 101);
+        Student student = new Student("A", 100);
         System.out.println("Name: " + student.getName());
         System.out.println("Roll: " + student.getRoll());
 
         Calculator calculator = new Calculator();
-        System.out.println("Addition: " + calculator.add(5, 2));
-        System.out.println("Subtraction: " + calculator.subtract(5, 2));
-        System.out.println("Multiplication: " + calculator.multiply(5, 2));
-        System.out.println("Division: " + calculator.divide(5, 2));
+        System.out.println("Addition: " + calculator.add(15, 3));
+        System.out.println("Subtraction: " + calculator.subtract(15, 3));
+        System.out.println("Multiplication: " + calculator.multiply(15, 3));
+        System.out.println("Division: " + calculator.divide(15, 3));
     }
 }
