73c8e327da8d99519eeaa0380b9719b6c939ad7c
Md. Aiful Alam Mahim <arifulalam7865@gmail.com>
2025-09-20 21:51:15 +0600

    message 1

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private String name;
+    private int roll;
+
+    Student(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+}
