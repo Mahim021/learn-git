3a23a3302b5958b66aacc54857beb6e8ec1af5ae
Md. Aiful Alam Mahim <arifulalam7865@gmail.com>
2025-09-20 23:01:23 +0600

    almost solved

--- main.java
+++ main.java
@@ -1,34 +1,16 @@
 public class main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
         Student student = new Student("A", 100);
-=======
         Student student = new Student("A", 100);
->>>>>>> B_B
-=======
-        Student student = new Student("A", 100);
->>>>>>> B_B
+
         System.out.println("Name: " + student.getName());
         System.out.println("Roll: " + student.getRoll());
 
         Calculator calculator = new Calculator();
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> B_B
+
         System.out.println("Addition: " + calculator.add(15, 3));
         System.out.println("Subtraction: " + calculator.subtract(15, 3));
         System.out.println("Multiplication: " + calculator.multiply(15, 3));
         System.out.println("Division: " + calculator.divide(15, 3));
-<<<<<<< HEAD
-=======
-        System.out.println("Addition: " + calculator.add(15, 3));
-        System.out.println("Subtraction: " + calculator.subtract(15, 3));
-        System.out.println("Multiplication: " + calculator.multiply(15, 3));
-        System.out.println("Division: " + calculator.divide(15, 3));
->>>>>>> B_B
-=======
->>>>>>> B_B
     }
 }
