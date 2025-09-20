b7a0c822e19c0dcdd8ea2b7af4b6040643a298e2
Md. Aiful Alam Mahim <arifulalam7865@gmail.com>
2025-09-20 21:36:45 +0600

    Added Student Class.java

---
+++ Java/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private string name;
+    private int roll;
+
+    Student(string name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+}
---
+++ Java/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        Student student = new Student("Alice", 101);
+        System.out.println("Name: " + student.getName());
+        System.out.println("Roll: " + student.getRoll());
+    }
+}
